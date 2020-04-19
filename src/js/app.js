21d8081d0e865a4ec519e579dbf1f3eee7f19d31
Vel Murugan <vel@fabevy.com>
2020-04-19 23:28:07 +0530

    header functionalities

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,134 @@
-// Core
-import Util from 'bootstrap/js/src/util'
-
-// Components
-import Alert from 'bootstrap/js/src/alert'
-import Button from 'bootstrap/js/src/button'
-import Carousel from 'bootstrap/js/src/carousel'
-import Collapse from 'bootstrap/js/src/collapse'
-import Dropdown from 'bootstrap/js/src/dropdown'
-import Modal from 'bootstrap/js/src/modal'
-import Popover from 'bootstrap/js/src/popover'
-import Scrollspy from 'bootstrap/js/src/scrollspy'
-import Tab from 'bootstrap/js/src/tab'
-import Toast from 'bootstrap/js/src/toast'
-import Tooltip from 'bootstrap/js/src/tooltip'
-
+var siteMapData = {
+	"configuration-management":{
+		"title": "Configuration Management",
+		"sub-menu":[
+			{
+				"name": "Client Configuration",
+				"link": "pages/client-cofig.html",
+				"sub-links":[]
+			},
+			{
+				"name": "Terminology Mapping",
+				"link": "pages/termi-map.html",
+				"sub-links":[]
+			},
+			{
+				"name": "Master Data",
+				"link": "pages/master-data.html",
+				"sub-links":[
+					{
+						"name": "Variable Master",
+						"link": "pages/termi-map.html"
+					},
+					{
+						"name": "Variable Master",
+						"link": "pages/termi-map.html"
+					},
+					{
+						"name": "Variable Master",
+						"link": "pages/termi-map.html"
+					},
+					{
+						"name": "Variable Master",
+						"link": "pages/termi-map.html"
+					},
+					{
+						"name": "Variable Master",
+						"link": "pages/termi-map.html"
+					},
+					{
+						"name": "Variable Master",
+						"link": "pages/termi-map.html"
+					},
+					{
+						"name": "Variable Master",
+						"link": "pages/termi-map.html"
+					},
+					{
+						"name": "Variable Master",
+						"link": "pages/termi-map.html"
+					},
+					{
+						"name": "Variable Master",
+						"link": "pages/termi-map.html"
+					},
+					{
+						"name": "Variable Master",
+						"link": "pages/termi-map.html"
+					},
+					{
+						"name": "Variable Master",
+						"link": "pages/termi-map.html"
+					},
+					{
+						"name": "Variable Master",
+						"link": "pages/termi-map.html"
+					},
+					{
+						"name": "Variable Master",
+						"link": "pages/termi-map.html"
+					},
+					{
+						"name": "Variable Master",
+						"link": "pages/termi-map.html"
+					},
+					{
+						"name": "Variable Master",
+						"link": "pages/termi-map.html"
+					},
+					{
+						"name": "Variable Master",
+						"link": "pages/termi-map.html"
+					},
+					{
+						"name": "Variable Master",
+						"link": "pages/termi-map.html"
+					},
+					{
+						"name": "Variable Master",
+						"link": "pages/termi-map.html"
+					},
+					{
+						"name": "Variable Master",
+						"link": "pages/termi-map.html"
+					},
+					{
+						"name": "Variable Master",
+						"link": "pages/termi-map.html"
+					},
+					{
+						"name": "Variable Master",
+						"link": "pages/termi-map.html"
+					},
+					{
+						"name": "Variable Master",
+						"link": "pages/termi-map.html"
+					},
+					{
+						"name": "Variable Master",
+						"link": "pages/termi-map.html"
+					},
+					{
+						"name": "Variable Master",
+						"link": "pages/termi-map.html"
+					},
+					{
+						"name": "Variable Master",
+						"link": "pages/termi-map.html"
+					},
+					{
+						"name": "Variable Master",
+						"link": "pages/termi-map.html"
+					},
+					{
+						"name": "Variable Master End",
+						"link": "pages/termi-map.html"
+					}
+				]
+			}
+		]
+	}
+}
 
 $(document).ready(function(){
   $(".dropdown-menu li").click(function(){
@@ -21,5 +136,58 @@ $(document).ready(function(){
     $(this).parents('.drop-down-box').find('.dropdown-toggle').html(selText+' <i class="fas fa-angle-down"></i>');
   });
 
-});
+  $(".menu-box").click(function(e){
+    e.preventDefault();
+    $(".main-nav-list").show();
+    $('.main-menu').hide();
+    //return;
+    var target = $(this).data('target'),
+    menuItems = siteMapData[target]['sub-menu'],
+    $menuParent = $('.main-nav-list'),
+    $menu = $("<ul/>"),
+    $list, $subMenu, $subMenuList, $navList, $navTitle;
 
+    //Form Title
+    $navTitle = $menuParent.find('.nav-title').find('h3');
+    $navTitle.text(siteMapData[target]['title'])
+
+    //Form Navigation
+    $navList = $menuParent.find('.nav-list');
+    $navList.html("");
+
+    menuItems.forEach(function(menuItem, index){
+
+      $list = $("<li><a href='"+menuItem.link+"' class='map-txt'>"+menuItem.name+"</a></li>");
+      
+      if (menuItem['sub-links'].length){
+
+        $list.find('a').removeClass('map-txt').addClass('data-txt');
+        $subMenu = $("<ul/>");
+        
+        menuItem['sub-links'].forEach(function(subLink, ind){
+          $subMenuList = $("<li><a href='"+subLink.link+"'>"+subLink.name+"</a></li>");
+          $subMenu.append($subMenuList);
+        });
+        
+        $list.append($subMenu); 
+      }
+      $menu.append($list);
+    });
+
+    $navList.append($menu);
+    $(".main-nav-list").show();
+    $('.main-menu').hide();
+
+    $(".nav-list").mCustomScrollbar({
+      setHeight: $(window).outerHeight() - $(".nav-title").outerHeight() - $('.header-sec').outerHeight(),
+      theme:"minimal-dark"
+    });
+  });
+  $('.back-btn').click(function(e){
+    e.preventDefault();
+    $(".main-nav-list").hide();
+    $('.main-menu').show();
+    return false;
+  })
+
+});
